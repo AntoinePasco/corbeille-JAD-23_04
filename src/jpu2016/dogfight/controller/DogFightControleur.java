b38d1b6e97bd8ed6754bae55c905e3fbd7720d68
AntoinePasco <antoine.pasco.5@gmail.com>
2018-04-24 11:18:33 +0200

    phase 1.5

--- src/jpu2016/dogfight/controller/DogFightControleur.java
+++ src/jpu2016/dogfight/controller/DogFightControleur.java
@@ -1,5 +1,14 @@
 package jpu2016.dogfight.controller;
 
-public class DogFightControleur implements jpu2016.dogfight.controller.IOrderPerformer {
+public class DogfightControleur implements IOrderPerformer {
+private int TIME_SLEEP = 30;
+void orderPerform(UserOrder userOrder){
 
+}
+public void play(){
+
+}
+
+public DogfightControleur(IDogfightModel dogfightModel) {
+    }
 }
--- src/jpu2016/dogfight/controller/IUserOrder.java
+++ src/jpu2016/dogfight/controller/IUserOrder.java
@@ -1,5 +1,6 @@
 package jpu2016.dogfight.controller;
 
 public interface IUserOrder {
-    public void issou();
+    void getOrder(Order order);
+    void getPlayer(int Player);
 }
